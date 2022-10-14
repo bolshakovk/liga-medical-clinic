7bbf7b40dc1140903a24459e44f14e2af23d6900
bolshakovk <bolshakovk1234@gmail.com>
2022-10-14 10:24:57 +0300

    4 hometask

--- core/src/main/java/liga/medical/messageanalyzer/core/MessageAnalyzer.java
+++ core/src/main/java/liga/medical/messageanalyzer/core/MessageAnalyzer.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-//TODO здесь сидит туду "liga.medical.common.service"
 @SpringBootApplication
-@ComponentScan(basePackages = {"liga.medical.messageanalyzer"})
+@ComponentScan(basePackages = {"liga.medical.messageanalyzer", "liga.medical.common.service"})
+
 public class MessageAnalyzer {
     public static void main(String[] args) {
         SpringApplication.run(MessageAnalyzer.class, args);
